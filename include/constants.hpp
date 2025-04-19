6a8ce030f8ca366bb9283cd807374093a70a6ca2
Souvik Naskar <shouvik0104@gmail.com>
2025-04-20 03:15:03 +0530

    startup animation polishing

--- include/constants.hpp
+++ include/constants.hpp
@@ -9,11 +9,22 @@
 namespace object {
 	inline constexpr std::string_view FISH = "\x1B[1;44;37m fish \x1B[0m";
 	inline constexpr const char* CURSOR = "\u2588";
+	inline constexpr const char* PLAY = "\u25BA";
+	inline constexpr const char* R = "\u2500";
+	inline constexpr const char* RLOWER = "_";
+	inline constexpr const char* RD = "\u2510";
+	inline constexpr const char* UR = "\u250C";
+	inline constexpr const char* RU = "\u2518";
+	inline constexpr const char* DR = "\u2514";
 }
 
 namespace action {
 	inline constexpr auto clear = []() {
-		std::cout << "\x1B[2J\x1B[H"; 
+		std::cout << "\x1B[2J"; 
+	};
+
+	inline constexpr auto home = []() {
+		std::cout << "\x1B[H"; 
 	};
 
 	inline constexpr auto reset_style = []() {
@@ -62,6 +73,22 @@ namespace action {
 
 	inline constexpr auto backspace = []() {
 		std::cout << "\x08";
+	};
+
+	inline constexpr auto up = [](int n) {
+		std::cout << "\x1B[" << n << "A";
+	};
+
+	inline constexpr auto down = [](int n) {
+		std::cout << "\x1B[" << n << "B";
+	};
+
+	inline constexpr auto forward = [](int n) {
+		std::cout << "\x1B[" << n << "C";
+	};
+
+	inline constexpr auto back = [](int n) {
+		std::cout << "\x1B[" << n << "D";
 	};
 
 	inline constexpr auto write_char = [](const char* c, const char* cursor, int sleep_ms) {
--- main.cpp
+++ main.cpp
@@ -9,24 +9,13 @@ void play_startup_animation(int term_width, int term_height) {
 	int center_y = term_height / 2;
 
 	action::clear();
+	action::home();
 	action::hide_cursor();
 	action::go_to(center_y, center_x - 7);
 	action::bold();
-
 	action::set_blinking();
-	action::flush(object::CURSOR, 1000);
 
-	action::backspace();
-	action::color(ForegroundColor::BRIGHT_MAGENTA, BackgroundColor::DEFAULT);
-	action::flush(object::CURSOR, 1000);
-
-	action::write_char("~", object::CURSOR, 200);
-	action::write_char("~", object::CURSOR, 200);
-	action::write_char("~", object::CURSOR, 200);
-	action::write_char(" ", object::CURSOR, 1000);
-
-	action::backspace();
-	action::color(ForegroundColor::BRIGHT_BLUE, BackgroundColor::DEFAULT);
+	action::color(ForegroundColor::BRIGHT_YELLOW, BackgroundColor::DEFAULT);
 	action::flush(object::CURSOR, 1000);
 
 	action::write_char("f", object::CURSOR, 200);
@@ -38,6 +27,7 @@ void play_startup_animation(int term_width, int term_height) {
 	action::italic();
 	action::color(ForegroundColor::BRIGHT_WHITE, BackgroundColor::BLUE);
 
+	action::write_char(" ", object::CURSOR, 200);
 	action::write_char("t", object::CURSOR, 200);
 	action::write_char("e", object::CURSOR, 200);
 	action::write_char("r", object::CURSOR, 200);
@@ -45,23 +35,39 @@ void play_startup_animation(int term_width, int term_height) {
 	action::write_char("i", object::CURSOR, 200);
 	action::write_char("n", object::CURSOR, 200);
 	action::write_char("a", object::CURSOR, 200);
-	action::write_char("l", object::CURSOR, 200);
-
-	action::color(ForegroundColor::BRIGHT_WHITE, BackgroundColor::DEFAULT);
-	action::write_char(" ", object::CURSOR, 1000);
+	action::write_char("l", object::CURSOR, 500);
 
 	action::backspace();
-	action::color(ForegroundColor::BRIGHT_MAGENTA, BackgroundColor::DEFAULT);
-	action::flush(object::CURSOR, 1000);
-
-	action::write_char("~", object::CURSOR, 200);
-	action::write_char("~", object::CURSOR, 200);
-	action::write_char("~", object::CURSOR, 3000);
-
-	action::unset_blinking();
 	action::reset_style();
+	action::unset_blinking();
+	action::color(ForegroundColor::BRIGHT_BLUE, BackgroundColor::DEFAULT);
+
+	action::flush(" ", 100);
+	action::flush(object::R, 100);
+	action::flush(object::R, 100);
+	action::flush(object::R, 100);
+	action::flush(object::RD, 100);
+	action::flush(object::RLOWER, 100);
+	action::flush(object::RLOWER, 100);
+	action::flush(object::RLOWER, 100);
+	action::flush(object::UR, 100);
+	action::flush(object::R, 100);
+	action::flush(object::R, 100);
+	action::flush(object::R, 100);
+	action::flush(object::RU, 100);
+	action::up(1);
+	action::flush(object::RLOWER, 100);
+	action::flush(object::RLOWER, 100);
+	action::flush(object::RLOWER, 100);
+	action::down(1);
+	action::flush(object::DR, 100);
+	action::flush(object::R, 100);
+	action::flush(object::R, 100);
+	action::flush(object::R, 3000);
+
 	action::show_cursor();
 	action::clear();
+	action::home();
 }
 
 
